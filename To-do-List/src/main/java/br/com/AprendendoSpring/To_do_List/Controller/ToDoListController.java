b735638b4347b72bfe6ea8e97000df6f863839e2
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-04-23 18:33:33 -0400

    controller feito

--- To-do-List/src/main/java/br/com/AprendendoSpring/To_do_List/Controller/ToDoListController.java
+++
@@ -1,5 +0,0 @@
-package br.com.AprendendoSpring.To_do_List.Controller;
-
-public class ToDoListController {
-
-}
---
+++ To-do-List/src/main/java/br/com/AprendendoSpring/To_do_List/User/UserController.java
@@ -0,0 +1,5 @@
+package br.com.AprendendoSpring.To_do_List.User;
+
+public class UserController {
+
+}
