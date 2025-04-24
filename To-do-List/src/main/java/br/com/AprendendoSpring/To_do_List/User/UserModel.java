1006b4b3a589291ce57d248d0bebc4e8fc9aa749
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-04-24 19:16:23 -0400

    tabela de usuarios criada

--- To-do-List/src/main/java/br/com/AprendendoSpring/To_do_List/User/UserModel.java
+++ To-do-List/src/main/java/br/com/AprendendoSpring/To_do_List/User/UserModel.java
@@ -1,13 +1,35 @@
 package br.com.AprendendoSpring.To_do_List.User;
 
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
+import jakarta.persistence.GeneratedValue;
+import java.time.LocalDateTime;
 
 @Data
+
+// Anotação para indicar que essa classe é uma entidade do banco de dados
+// O nome da tabela será "tb_users"
+@Entity(name = "tb_users")
 public class UserModel {
+
+  @Id
+
+  // Gera o ID automaticamente
+  @GeneratedValue(generator = "UUID")
+
+  private UUID id;
 
   // Atributos do usuario
   private String name;
   private String email;
   private String password;
+
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
 }
